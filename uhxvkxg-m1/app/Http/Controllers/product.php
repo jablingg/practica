8c7704675355a6fe5d2a4e30900f8861a338a95f
jablingg <pinmaza228@gmail.com>
2023-02-16 18:44:03 -0800

    first commit

---
+++ uhxvkxg-m1/app/Http/Controllers/product.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\products;
+use Illuminate\Http\Request;
+
+//class product extends Controller
+//
+   //public function slider()
+  // {
+  //  $a =product:: all();
+//return view('about',['a' => $a]);
+   //}
+//}
